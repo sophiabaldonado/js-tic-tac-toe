a95341334584022884a53cd3d3e3bdadd246ad3b
sophia <sophiabaldonado@gmail.com>
2016-06-30 12:49:48 -0700

    Get id from space that is clicked;

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,46 @@
+// what is a turn?
+
+// determine which player has a turn
+
+// select a space
+ // space available?
+ // space advantage
+// claim space using player identifier
+// check win/loss state
+ // what is a win?
+ // what is a draw?
+ // what is a loss?
+ // communicate outcome
+ // continue play?
+
 function TicTacToe() {
+  this.claim = function (space_id) {
+    console.log('claimed: ', space_id)
+  }
 
 }
 
 TicTacToe.prototype = {
-  
 }
+
+// jQ
+$(document).on('ready', function() {
+  console.log('create and begin the game here!')
+
+  var game = new TicTacToe()
+  var board = $('.board')
+  var spaces = $('.space')
+
+  // claiming a space
+
+  var player = 'player1'
+
+  spaces.click(function (event) {
+    event.preventDefault()
+
+    game.claim($(this).attr('id'))
+    $(this).addClass(player)
+  })
+
+
+})
